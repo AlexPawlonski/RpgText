358d5206b3b2adfb9e3c281d5879853a5061ccf1
AlexPawlonski <apawlonski8@hotmail.com>
2020-09-28 16:54:04 +0200

    ajout de la morte

--- src/Class/moteur.js
+++ src/Class/moteur.js
@@ -65,7 +65,7 @@ class Moteur extends React.Component{
                 }
             });
 
-            let index14 = ['regarder', 'autoure', 'au alentoure','dans les barrage']
+            let index14 = ['regarder','regarde', 'autoure', 'au alentoure','dans les barrage']
             index14.forEach(element => {
                 if(input.includes(element)){
                     let save = 6;
@@ -144,17 +144,24 @@ class Moteur extends React.Component{
         if(this.props.save == 4){
             let input = this.props.result
 
-            let index75 = ['toucher', 'éffleurer ', 'touche']
-            index75.forEach(element => {
+            let index41 = ['toucher', 'éffleurer ', 'touche', 'prend', 'jette dessus']
+            index41.forEach(element => {
+                if(input.includes(element)){
+                    document.location.reload(true);
+                }
+            });
+
+            let index42 = ['éloignier', 'fuire','cour', 'part', 'par', 'pare', 'recule']
+            index42.forEach(element => {
                 if(input.includes(element)){
                     
-
                 }
             });
             return(
                 <ul>
                     <li>Avec assurance vous toucher la pierre, ça lueur augementa d'un coup ! </li>
-                    <li>Un peu éblouie vous vous cacher les yeux avec votre bras </li>
+                    <li>Un peu éblouie vous vous cacher les yeux avec votre bras, surpprit vous reculer d'un pas !</li>
+                    <li>Que faite vous ?</li>
                 </ul>
             );
 
@@ -176,7 +183,7 @@ class Moteur extends React.Component{
                     <li>Vous raviser et vous reculer d'un pas, mais pensant qu'une pierre qui brillait au fin fond d'une forait. Pouvait avoir son importance 
                         vour rercher dans votre petit packtage, un papier et un bou de charbon pour y noter les syboles inscri sur la pierre. </li>
                     <li> </li>
-                    <li>Puis voulant vous rendre au plus vite au prochain village vous retourner sur le chemain </li>
+                    <li>Puis voulant vous rendre au plus vite au prochain village vous retourner sur le chemain principale</li>
                 </ul>
             );
 
@@ -204,7 +211,37 @@ class Moteur extends React.Component{
             );
 
         }
+        //regarder autour de vous 
         if(this.props.save == 7){
+            let input = this.props.result
+
+            let index71 = ['pare', 'fuit', 'fui', 'éloigne', 'recule']
+            index71.forEach(element => {
+                if(input.includes(element)){
+                    let save = -2;
+                    this.props.fSave({save}); 
+                }
+            });
+            let index9 = ['toucher', 'jète dessus', 'touche', 'approche', 'ataque']
+            index9.forEach(element => {
+                if(input.includes(element)){
+                    let save = -3;
+                    this.props.fSave({save}); 
+                }
+            });
+            return(
+                <ul>
+                    <li>Vous être sur vos garde et vous regarder autour de vous, il n'y avais pas grand chose à votre droit un petit chemin qui ramenait a la route principale.
+                        A votre gauche la foret s'étendais sur des mille mais vous d'istingé une petite clairière, surment un endroit pour faire parurer les bète.
+                        Dérrier vous la foret, et devant vous la pierre, qui maintenant que vous y pérer atention, vous remarquer que cette pière resenblais a un vielle stelle polie par le temp. 
+                    </li>
+                    <li>Que faite vous ?</li>
+                </ul>
+            );
+
+        }
+        //la fuite de la pierre
+        if(this.props.save == 8){
             let input = this.props.result
 
             let index9 = ['toucher', 'éffleurer ', 'touche']
@@ -216,9 +253,8 @@ class Moteur extends React.Component{
             });
             return(
                 <ul>
-                    <li>Vous être sur vos garde et vous regarder autour de vous, il n'y avais pas grand chose à votre droit un petit chemin qui ramenait a la route principale.
-                        A votre gauche la foret s'étendais sur des mille mais vous d'istingé une petite clairière, surment un endroit pour faire parurer les bète.
-                    </li>
+                    <li>vous fuyer r'apidament de l'emplacement de la la pierre, sans vous retourner vous courer dans une direction inconue, car dans votre fuite vous n'avier pas eu le temps de savoir un vous alier.
+                        Par un heureux assare ou par chance vous  </li>
                 </ul>
             );
 
@@ -228,6 +264,35 @@ class Moteur extends React.Component{
 
 
 
+
+
+
+//mort 
+    if(this.props.save == 1000){
+        let input = this.props.result
+        let index1001 = ['yes', 'oui']
+        index1001.forEach(element => {
+            if(input.includes(element)){
+                let save = -2;
+                this.props.fSave({save}); 
+            }
+        });
+
+        let index1002 = ['no', 'nom','nop']
+        index1002.forEach(element => {
+            if(input.includes(element)){
+                document.location.reload(true);
+            }
+        });
+
+        return(
+            <ul>
+                <li>Vous être mort! vous n'avez pas sus venir a bou de Zaxarot !!</li>
+                <li>voulez vous revivre ?</li>
+            </ul>
+        ); 
+    }   
+
 //Event erreur de compréhention 
         if(this.props.save == 404){
             return(
@@ -236,6 +301,7 @@ class Moteur extends React.Component{
                 </ul>
             );
         }
+
 
     };
 }
